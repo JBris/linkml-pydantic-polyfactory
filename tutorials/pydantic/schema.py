71a1ff60db1b59275c9bc57ddf59012070622f7d
James Bristow <JBris@users.noreply.github.com>
2025-07-14 00:27:23 +1200

    Update schema.py

--- tutorials/pydantic/schema.py
+++ tutorials/pydantic/schema.py
@@ -7,13 +7,13 @@ class TimeSeriesPoint(BaseModel):
         description="The timestamp"
     )
     value: float = Field(
-        description="The timestamp",
+        description="The time series value",
         unit="cm",
         ge=0
     )
     
-    source: Literal["sensor-A"] = Field(default="sensor-A")
-    unit: Literal["Celsius"] = Field(default="Celsius")
+    source: Literal["sensor-A"] = Field(default="sensor-A", frozen=True)
+    unit: Literal["Celsius"] = Field(default="Celsius", frozen=True)
     num: Literal[1] = Field(default=1, frozen=True)
 
 point = TimeSeriesPoint(timestamp=datetime.now(), value=42.0)
